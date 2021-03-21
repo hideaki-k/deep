00c99779bba7835fc44bdc62d4834c615c48ea1b
hideaki-k <h.kinoshita@kimura-lab.net>
2021-03-21 19:38:51 +0900

    クレータ内＆外縁

--- DEM/make_dem.m
+++ DEM/make_dem.m
@@ -1,42 +1,54 @@
 size_factor = 500
 center = size_factor/2
 model = zeros(size_factor,size_factor);
+R = 100
+H_r = 5
+H_c = 10
+
+W_r = 200
 for i =  1:1:size_factor
     for j = 1:1:size_factor
-        f = abs(i-center)^2 + abs(j-center)^2;
-        model(i,j) = f;
+        
+
+        r = sqrt(abs(i-center)^2 + abs(j-center)^2);
+        if r <= R
+            h = (H_c+H_r)*r^2/R^2;
+        else
+            h = H_r*(R+W_r)^3/((R+W_r)^3-R^3)*(r/R)^(-3) - (H_r*R^3)/((R+W_r)^3-R^3);
+        end
+        model(i,j) = h;
     end
 end
 model
 
 
+% 
+% v = VideoWriter('peaks_1.avi');
+% open(v);
+% lidar_data = zeros(size_factor,size_factor);
+% for i =  1:1:size_factor*10
+%   
+%     lidar_data(model==i) = 1;
+%     
+%     if rem(i,10) == 0
+%         imagesc(lidar_data);
+%         colorbar;
+%         hold on;
+%         pause(0.1)
+%         frame = lidar_data;
+%         writeVideo(v,frame);
+%         lidar_data =  zeros(size_factor,size_factor);
+%     end
+%     
+% 
+%     
+% end
+% close(v);
+% imagesc(lidar_data);
+% colorbar;
 
-v = VideoWriter('peaks_1.avi');
-open(v);
-lidar_data = zeros(size_factor,size_factor);
-for i =  1:1:size_factor*10
-  
-    lidar_data(model==i) = 1;
-    
-    if rem(i,10) == 0
-        imagesc(lidar_data);
-        colorbar;
-        hold on;
-        pause(0.1)
-        frame = lidar_data;
-        writeVideo(v,frame);
-        lidar_data =  zeros(size_factor,size_factor);
-    end
-    
-
-    
-end
-close(v);
-imagesc(lidar_data);
-colorbar;
-
-imagesc(model);
-colorbar;
+% imagesc(model);
+% colorbar;
 % f1 = figure;
 % f2 = figure;
 % figure(f1);
@@ -47,5 +59,6 @@ colorbar;
 
 s = surface(model);
 s.EdgeColor = 'none';
+zlim([0 50])
 %colorbar
 view(3)
