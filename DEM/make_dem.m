882581c375f9f180243a9a154803d5113d76422f
hideaki-k <h.kinoshita@kimura-lab.net>
2021-03-24 01:45:53 +0900

    クレータ完成

--- DEM/make_dem.m
+++ DEM/make_dem.m
@@ -1,25 +1,38 @@
 size_factor = 500
 center = size_factor/2
 model = zeros(size_factor,size_factor);
-R = 100
-H_r = 5
-H_c = 10
 
-W_r = 200
+
+H_r = 3
+H_ro = 0.036*(2*R)^1.014
+H_r = H_ro
+H_c = 0.196*(2*R)^1.010 - H_ro
+W_r = 0.257*(2*R)^1.011
+% RANGE
+R = 100
+alpha = (H_c+H_r)*R/(H_c+H_ro)
+beta = R+(1-(H_c+H_r)/(H_c+H_ro))*W_r
+
+A = -3*R^3 + 2*R^2*beta + 2*R*beta^2 + 2*beta^3
+
 for i =  1:1:size_factor
     for j = 1:1:size_factor
         
 
         r = sqrt(abs(i-center)^2 + abs(j-center)^2);
-        if r <= R
-            h = (H_c+H_r)*r^2/R^2;
-        else
+         if r <= alpha
+             h = (H_c+H_ro)*(r^2/R^2)-H_c;
+         elseif r <= R
+             h = ((H_c + H_ro)^2/(H_r - H_ro) - H_c)*((r/R) - 1)^2 + H_r;
+         elseif r < beta
+             h =  (H_r*(R+W_r)^3*A)/(W_r*beta^4*(R-beta)^2*(3*R^2+3*R*W_r+W_r^2)) * (r-R)^2*(r-beta*(1+(beta^3-R^3)/A))+H_r;
+        else    
             h = H_r*(R+W_r)^3/((R+W_r)^3-R^3)*(r/R)^(-3) - (H_r*R^3)/((R+W_r)^3-R^3);
         end
         model(i,j) = h;
     end
 end
-model
+model;
 
 
 % 
@@ -59,6 +72,6 @@ model
 
 s = surface(model);
 s.EdgeColor = 'none';
-zlim([0 50])
+zlim([-50 50])
 %colorbar
 view(3)
