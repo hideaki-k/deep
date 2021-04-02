25fe2f51496dba6f8d281ba39683b6b41207cd41
hideaki-k <h.kinoshita@kimura-lab.net>
2021-04-02 12:57:55 +0900

    ノイズ追加

--- DEM/make_crater.m
+++ DEM/make_crater.m
@@ -1,15 +1,16 @@
-size_factor = 500
+size_factor = 128
 center = size_factor/2
 model = zeros(size_factor,size_factor);
 
 
-H_r = 3
+R = 30 + 50*rand(1)
+H_r = 10
 H_ro = 0.036*(2*R)^1.014
 H_r = H_ro
-H_c = 0.196*(2*R)^1.010 - H_ro
+H_c = 0.196*(2*R)^1.010 - H_ro 
 W_r = 0.257*(2*R)^1.011
 % RANGE
-R = 100
+
 alpha = (H_c+H_r)*R/(H_c+H_ro)
 beta = R+(1-(H_c+H_r)/(H_c+H_ro))*W_r
 
@@ -28,35 +29,47 @@ for i =  1:1:size_factor
              h =  (H_r*(R+W_r)^3*A)/(W_r*beta^4*(R-beta)^2*(3*R^2+3*R*W_r+W_r^2)) * (r-R)^2*(r-beta*(1+(beta^3-R^3)/A))+H_r;
         else    
             h = H_r*(R+W_r)^3/((R+W_r)^3-R^3)*(r/R)^(-3) - (H_r*R^3)/((R+W_r)^3-R^3);
-        end
-        model(i,j) = h;
+         end
+        y = wgn(1,1,0);
+        model(i,j) = h+y;
     end
 end
 model;
+model = round(model,0);
+model;
+% model = model.*10000
+% model
+a = max(model(:)) %22013
+b = min(model(:)) %5885
 
+s = surface(model);
+s.EdgeColor = 'none';
+zlim([-50 50])
+%colorbar
+view(3)
 
-% 
-% v = VideoWriter('peaks_1.avi');
-% open(v);
-% lidar_data = zeros(size_factor,size_factor);
-% for i =  1:1:size_factor*10
-%   
-%     lidar_data(model==i) = 1;
+v = VideoWriter('peaks_1.avi');
+open(v);
+lidar_data = zeros(size_factor,size_factor);
+
+for i =  a:-1:b
+   
+    lidar_data(model==i) = 1;
 %     
-%     if rem(i,10) == 0
-%         imagesc(lidar_data);
-%         colorbar;
-%         hold on;
-%         pause(0.1)
-%         frame = lidar_data;
-%         writeVideo(v,frame);
-%         lidar_data =  zeros(size_factor,size_factor);
-%     end
-%     
-% 
-%     
-% end
-% close(v);
+%      if rem(i,100) == 0
+        imagesc(lidar_data);
+        colorbar;
+        hold on;
+        pause(0.1)
+        frame = lidar_data;
+        writeVideo(v,frame);
+        lidar_data =  zeros(size_factor,size_factor);
+%      end
+    
+
+    
+end
+close(v);
 % imagesc(lidar_data);
 % colorbar;
 
@@ -70,8 +83,3 @@ model;
 % imagesc(trim);colorbar;
 
 
-s = surface(model);
-s.EdgeColor = 'none';
-zlim([-50 50])
-%colorbar
-view(3)
