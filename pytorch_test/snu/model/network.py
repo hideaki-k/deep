75b7f5f1da655c838540972d1e5ee23fde87842c
hideaki-k <h.kinoshita@kimura-lab.net>
2021-04-22 06:04:09 +0900

    畳み込み追加（べた書き）

--- pytorch_test/snu/model/network.py
+++ pytorch_test/snu/model/network.py
@@ -133,11 +133,12 @@ class SNU_Network_classification(torch.nn.Module):
         
     def forward(self, x, y):
         loss = None
-        correct = 0
+        acc = 0
         sum_out = None
+        correct = 0
         dtype = torch.float
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        out = torch.zeros((256,self.n_out), device=device, dtype=dtype)
+        out = torch.zeros((128,self.n_out), device=device, dtype=dtype)
         out_rec = [out]
         log_softmax_fn = nn.LogSoftmax(dim=1)
         loss_fn = nn.NLLLoss()
@@ -150,13 +151,13 @@ class SNU_Network_classification(torch.nn.Module):
             out_list = []
         
         for t in range(self.num_time):
-            print("**********************")
-            print("t :",t)
-            print("x[0]  shape:",x[0].shape)
+            #print("**********************")
+            #print("t :",t)
+            #print("x[0]  shape:",x[0].shape)
             x_t = x[:,:,t]  #torch.Size([256, 784])
             
-            print("x_t[0] shape",x_t[0].shape)
-            print("sum x_t[0]",sum(x_t[0]))
+            #print("x_t[0] shape",x_t[0].shape)
+            #print("sum x_t[0]",sum(x_t[0]))
             
            
             h1 = self.l1(x_t) # torch.Size([256, 256])
@@ -167,8 +168,8 @@ class SNU_Network_classification(torch.nn.Module):
             h3 = self.l3(h2)
             #print("sum h3[0]",sum(h3[0]))
             out = self.l4(h3) #out.shape torch.Size([256, 10]) # [バッチサイズ,output.shape]
-            print("out.shape",out.shape) #out[0].shape torch.Size([10])
-            print("sum out[0]:",sum(out[0]))  #tensor([1., 0., 1., 0., 1., 0., 1., 1., 0., 1.], device='cuda:0',
+            #print("out.shape",out.shape) #out[0].shape torch.Size([10])
+            #print("out[0]:",out[0])  #tensor([1., 0., 1., 0., 1., 0., 1., 1., 0., 1.], device='cuda:0',
 
             
             if self.test_mode == True:
@@ -181,22 +182,36 @@ class SNU_Network_classification(torch.nn.Module):
             out_rec.append(out)
     
         out_rec = torch.stack(out_rec,dim=1)
-        print("out_rec.shape",out_rec.shape) #out_rec.shape torch.Size([256, 11, 10]) ([バッチ,時間,分類])
+        #print("out_rec.shape",out_rec.shape) #out_rec.shape torch.Size([256, 11, 10]) ([バッチ,時間,分類])
         #m,_=torch.sum(out_rec,1)
         m =torch.sum(out_rec,1) #m.shape: torch.Size([256, 10])
-        print("m.shape:",m.shape)
-        print("m",m)
+        m = m/20
+        #print("type m:",m.type())
+        #print("m",m)
         
-        print("out_rec.shape",out_rec.shape)
-        print("y.shape",y.shape)
+        #print("out_rec.shape",out_rec.shape)
+        y = torch.tensor(y, dtype=torch.int64)
+        #print("type y",y.type()) #torch.Size([128]))
+        #print("y",y)
 
 
         print("///////////////////////")
-        #criterion = nn.CrossEntropyLoss() #MNIST 
-        criterion = nn.MSELoss() # semantic segmantation
-         
+        criterion = nn.CrossEntropyLoss() #MNIST 
+        #criterion = nn.MSELoss() # semantic segmantation
+        _,m_col =  torch.max(m, 1)
+        #_,y_col = torch.max(y,1)
+        #acc = torch.sum(m_col == y_col) * 1.0 / len(y)
+        #acc = acc.to('cpu').detach().numpy().copy()
+        #print("correct : ",acc)
         loss = criterion(m, y)
-        print("loss",loss)
+        print("end of BCE loss :", loss)
+        #print("correct : ",acc)
+        #acc = torch.sum(m == y) * 1.0 / len(y)
+        y = F.one_hot(y,num_classes=2)
+        _,y_col = torch.max(y,1)
+        acc = torch.sum(m_col == y_col) * 1.0 / len(y)
+        acc = acc.to('cpu').detach().numpy().copy()
+        print("correct : ",acc)
         #loss += self.gamma*torch.sum(m**2)
         #print("gamma loss",loss)
         
@@ -206,4 +221,114 @@ class SNU_Network_classification(torch.nn.Module):
         if self.test_mode == True:
             return loss, accuracy, h1_list, h2_list, h3_list, out_list
         else:
-            return loss, m, out_rec
+            return loss, m, out_rec,acc
+
+# 新実装(4/21=)
+class Conv_SNU_Network_classification(torch.nn.Module):
+    def __init__(self, n_in=784, n_mid=256, n_out=2, filter = 10,
+                 num_time=20, l_tau=0.8, soft=False, gpu=False,
+                 test_mode=False):
+        super(Conv_SNU_Network_classification, self).__init__()
+        
+        # 入力チャネル数 出力チャネル数 フィルタサイズ
+        self.cn1 = snu_layer.Conv_SNU(1, 6, 10, l_tau=l_tau, soft=soft, gpu=gpu)
+        print("cn1!")
+        # 
+        self.l2 = snu_layer.SNU(55, 2, l_tau=l_tau, soft=soft, gpu=gpu)
+        print("l2!")
+        self.n_out = n_out
+        self.num_time = num_time
+        self.gamma = (1/(num_time*n_out))*1e-3
+        self.test_mode = test_mode
+
+    def _reset_state(self):
+        self.cn1.reset_state()
+        self.l2.reset_state()
+        
+    def forward(self, x, y):
+        loss = None
+        acc = 0
+        sum_out = None
+        correct = 0
+        dtype = torch.float
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        out = torch.zeros((128,self.n_out), device=device, dtype=dtype)
+        out_rec = [out]
+        log_softmax_fn = nn.LogSoftmax(dim=1)
+        loss_fn = nn.NLLLoss()
+        self._reset_state()
+
+        
+        if self.test_mode == True:
+            h1_list = []
+            h2_list = []
+            h3_list = []
+            out_list = []
+        
+        for t in range(self.num_time):
+
+            x_t = x[:,:,t]  #torch.Size([256, 784])
+            print("x_t : ",x_t.shape)
+            x_t = x_t.reshape((len(x_t), 1, 64, 64))
+            print("x_t : ",x_t.shape)
+
+            
+            # 第一層　畳み込み
+            h1 = self.cn1(x_t) # 
+            # 第二層 最大プーリング
+            h2 = F.max_pool2d(h1, 2)
+            h2 = torch.flatten(h2, 1)
+            # 第三層　出力
+            print("end flatten")
+            out = self.l2(h2)
+            
+            
+            if self.test_mode == True:
+                h1_list.append(h1)
+                h2_list.append(h2)
+                out_list.append(out)
+            
+            #sum_out = out if sum_out is None else sum_out + out
+            out_rec.append(out)
+    
+        out_rec = torch.stack(out_rec,dim=1)
+        #print("out_rec.shape",out_rec.shape) #out_rec.shape torch.Size([256, 11, 10]) ([バッチ,時間,分類])
+        #m,_=torch.sum(out_rec,1)
+        m =torch.sum(out_rec,1) #m.shape: torch.Size([256, 10])
+        m = m/20
+        #print("type m:",m.type())
+        #print("m",m)
+        
+        #print("out_rec.shape",out_rec.shape)
+        y = torch.tensor(y, dtype=torch.int64)
+        #print("type y",y.type()) #torch.Size([128]))
+        #print("y",y)
+
+
+        print("///////////////////////")
+        criterion = nn.CrossEntropyLoss() #MNIST 
+        #criterion = nn.MSELoss() # semantic segmantation
+        _,m_col =  torch.max(m, 1)
+        #_,y_col = torch.max(y,1)
+        #acc = torch.sum(m_col == y_col) * 1.0 / len(y)
+        #acc = acc.to('cpu').detach().numpy().copy()
+        #print("correct : ",acc)
+        loss = criterion(m, y)
+        print("end of BCE loss :", loss)
+        #print("correct : ",acc)
+        #acc = torch.sum(m == y) * 1.0 / len(y)
+        y = F.one_hot(y,num_classes=2)
+        _,y_col = torch.max(y,1)
+        acc = torch.sum(m_col == y_col) * 1.0 / len(y)
+        acc = acc.to('cpu').detach().numpy().copy()
+        print("correct : ",acc)
+        #loss += self.gamma*torch.sum(m**2)
+        #print("gamma loss",loss)
+        
+        
+
+        
+        if self.test_mode == True:
+            return loss, accuracy, h1_list, h2_list, h3_list, out_list
+        else:
+            return loss, m, out_rec,acc
