c32a321ee8fe1130187adf70072f438cd4d7e049
hideaki-k <h.kinoshita@kimura-lab.net>
2021-04-25 20:51:35 +0900

    tConv_SNUの追加

--- pytorch_test/snu/model/snu_layer.py
+++ pytorch_test/snu/model/snu_layer.py
@@ -168,9 +168,109 @@ class Conv_SNU(nn.Module):
         self.s = torch.zeros((shape[0], self.n_out, 55, 55),device=device,dtype=dtype)
         self.y = torch.zeros((shape[0], self.n_out, 55, 55),device=device,dtype=dtype)
         
+    
+    def forward(self,x):
+        if self.s is None:
+            #print("self.s is none")
+            self.initialize_state(x.shape)
 
+
+        if type(self.s) == numpy.ndarray:
+            self.s = torch.from_numpy(self.s.astype(np.float32)).clone()
+    
+        #print("x in snu:",x) 
+        print("x in snu.shape",x.shape) #x in snu.shape torch.Size([256, 784])
+        print("self.Wx(x).shape",self.Wx(x).shape)
+        
+        print("self.Wx(x).shape",self.Wx(x).shape)
+        print("self.s.shape : ",self.s.shape)
+        #print( " self.l_tau * self.s * (1-self.y)):",self.l_tau * self.s * (1-self.y))
+        #print("self.Wx(x):",self.Wx(x))
+        
+        s = F.elu(abs(self.Wx(x)) + self.l_tau * self.s * (1-self.y))
         
 
+        if self.soft:
+
+            axis = 1
+            bias_ = s + self.b[(...,) + (None,) * (s.ndim - self.b.ndim - axis)]
+            #print("bias_:",bias_)
+            y = F.sigmoid(bias_)
+
+        else:
+            axis = 0
+
+            #print("s.shape:", s.shape)
+            #print("self.b.shape:", self.b.shape)
+            #print("self.initial_bias.shape:",self.initial_bias.shape)
+            #print("self.b.shape !!!!!!!!!!!!!!!! ", self.b[(...,) + (None,) * (s.ndim - self.b.ndim - axis)].shape)
+            bias = s + self.b[(...,) + (None,) * (s.ndim - self.b.ndim - axis)] #error!! two types
+            #print("bias:",bias)
+            #print("s in snu:",s)
+            bias = s + self.b
+            #bias = s + self.initial_bias
+            
+            #print("bias in snu:",bias)
+            y = step_func.spike_fn(bias)
+        
+        self.s = s
+        self.y = y
+
+        return y
+
+
+class tConv_SNU(nn.Module):
+    def __init__(self, n_in, n_out, filter, l_tau=0.8, soft=False, rec=False, nobias=False, initial_bias=-0.5, gpu=True):
+        super(Conv_SNU,self).__init__()
+
+        self.n_in = n_in
+        self.n_out = n_out
+        self.filter = filter
+        self.l_tau = l_tau
+        self.rec = rec
+        self.soft = soft
+        self.gpu = gpu
+        self.s = None
+        self.y = None
+        self.initial_bias = initial_bias
+
+        if self.gpu:
+            #xp = cuda.cupy
+            dtype = torch.float
+            device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        else:
+            dtype = torch.float
+            device=torch.device("cpu")
+        
+
+        self.Wx = nn.ConvTranspose2d(n_in, n_out, filter, bias=False).to(device) #入力チャネル数, 出力チャネル数, フィルタサイズ
+        torch.nn.init.xavier_uniform_(self.Wx.weight)
+
+        if nobias:
+            self.b = None
+        else:
+
+            #print("initial_bias",initial_bias)
+            device = torch.device(device)
+            
+            self.b = nn.Parameter(torch.Tensor([initial_bias]).to(device))
+            #print("self.b",self.b)
+
+    def reset_state(self, s=None, y=None):
+        self.s = s
+        self.y = y
+
+    def initialize_state(self, shape):
+        if self.gpu:
+            #xp = cuda.cupy
+            dtype = torch.float
+            device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        else:
+            dtype = torch.float
+            device=torch.device("cpu")
+            
+        self.s = torch.zeros((shape[0], self.n_out, 55, 55),device=device,dtype=dtype)
+        self.y = torch.zeros((shape[0], self.n_out, 55, 55),device=device,dtype=dtype)
         
     
     def forward(self,x):
