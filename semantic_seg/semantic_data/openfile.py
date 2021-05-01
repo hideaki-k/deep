c59289263c8d265428bcc034ac716da5d98d4134
hideaki-k <h.kinoshita@kimura-lab.net>
2021-05-02 01:27:04 +0900

    Create openfile.py

---
+++ semantic_seg/semantic_data/openfile.py
@@ -0,0 +1,29 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from PIL import Image
+import matplotlib.animation as animation
+import time
+import scipy.io
+name = scipy.io.loadmat('image/img_100.mat')
+print(name)
+
+print(type(name))
+print(name.keys())
+print(name.values())
+print(name.items())
+print(name['name'])
+print(type(name['name']))
+print("============")
+print(name['name'].shape)
+print(name['name'][:,:,1])
+img = name['name'][:,:,1]
+N = 100
+fig, ax = plt.subplots()
+def update(i):
+    img = name['name'][:,:,i]
+    
+    plt.clf()
+    
+    plt.imshow(img,cmap='gray')
+hoge = animation.FuncAnimation(fig, update, np.arange(1,  N), interval=25)  # 代入しないと消される
+plt.show()
