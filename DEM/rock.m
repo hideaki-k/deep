1d35de91c2f7bb292ea0ff002c4025226f768c1c
hideaki-k <h.kinoshita@kimura-lab.net>
2021-05-09 00:00:23 +0900

    高解像に対応

--- DEM/rock.m
+++ DEM/rock.m
@@ -3,24 +3,24 @@
 %    (X-XC)^2     (Y-YC)^2     (Z-ZC)^2
 %    --------  +  --------  +  --------  =  1
 %      XR^2         YR^2         ZR^2
-size_factor = 5
-xc = 10;
-yc = 5;
+
+xc = 100;
+yc = 100;
 zc = 0;
-xr = 10;
-yr = 5;
+xr = 80;
+yr = 30;
 zr = 10;
-model = zeros(2*xr,2*yr);
-for i =  1:1:2*xr
-    for j = 1:1:2*yr
+model = zeros(200,200);
+for i =  1:1:200
+    for j = 1:1:200
        if j > yc
            if j < yc + yr*sqrt(1-(i-xc)^2/xr^2)
-                z = genrate_ellipsoid(i,j,xc, yc, zc, 10, 5, 10);
+                z = genrate_ellipsoid(i,j,xc, yc, zc, xr, yr, zr);
                 model(i,j) = z;
            end
        else
           if j > yc - yr*sqrt(1-(i-xc)^2/xr^2)
-                z = genrate_ellipsoid(i,j,xc, yc, zc, 10, 5, 10);
+                z = genrate_ellipsoid(i,j,xc, yc, zc, xr, yr, zr);
                 model(i,j) = z;
           end
        end
@@ -29,10 +29,12 @@ end
 s = surface(model);
 s.EdgeColor = 'none';
 zlim([0 30])
+xlim([0 200])
+ylim([0 200])
 %colorbar
 view(3)
 function z =genrate_ellipsoid(x ,y, xc, yc, zc, xr, yr, zr)
 x,y
-z =  zc + zr*sqrt(1-((x-xc)^2/xr^2)-((y-yc)^2/yr^2))
+z =  zc + zr*sqrt(1-((x-xc)^2/xr^2)-((y-yc)^2/yr^2));
 
 end
