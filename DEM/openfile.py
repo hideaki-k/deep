f14f4cdaee418b86522c7f8bbd75f448a200e161
hideaki-k <h.kinoshita@kimura-lab.net>
2021-05-25 18:04:50 +0900

    Update openfile.py

--- DEM/openfile.py
+++ DEM/openfile.py
@@ -5,19 +5,29 @@ import matplotlib.animation as animation
 import time
 import scipy.io
 #time_data = scipy.io.loadmat('crater/data_10.mat')
-time_data = scipy.io.loadmat('terrain_generation/perlin_bolder/data_10.mat')
+#time_data = scipy.io.loadmat('terrain_generation/perlin_bolder/data_10.mat')
+time_data = scipy.io.loadmat('two_craters_label/data_10.mat')
 print(time_data)
 
 print(type(time_data))
 print(time_data.keys())
 print(time_data.values())
 print(time_data.items())
+print(time_data['label_data'])
+
+print(time_data['label_data'].shape)
+plt.imshow(time_data['label_data'])
+plt.show()
+
+
+""""
 print(time_data['time_data'])
 print(type(time_data['time_data']))
 print(time_data['time_data'][:,:,1])
 img = time_data['time_data'][:,:,1]
 N = 100
 fig, ax = plt.subplots()
+
 def update(i):
     img = time_data['time_data'][:,:,i]
     
@@ -26,3 +36,4 @@ def update(i):
     plt.imshow(img,cmap='gray')
 hoge = animation.FuncAnimation(fig, update, np.arange(1,  N), interval=25)  # 代入しないと消される
 plt.show()
+"""
