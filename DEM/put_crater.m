1ea1cd67f2d5efb447c3bcf9e2c9dc5399269c80
hideaki-k <h.kinoshita@kimura-lab.net>
2021-05-25 18:04:59 +0900

    Update put_crater.m

--- DEM/put_crater.m
+++ DEM/put_crater.m
@@ -1,5 +1,5 @@
 function f = put_crater(k,mode)   
-    f = mode
+    f = mode;
     %% init parameter
     size_factor = 512;
     num_crater = 2;
@@ -8,7 +8,7 @@ function f = put_crater(k,mode)
     model = zeros(size_factor,size_factor);
 
 
-    R = 50 + abs(5*randn(1)) %クレータ半径
+    R = 50 + abs(5*randn(1)); %クレータ半径
     % H_r = 150 + abs(5*randn())
     H_ro = 0.036*(2*R)^1.014;
     H_r = H_ro;
@@ -16,18 +16,18 @@ function f = put_crater(k,mode)
     W_r = 0.257*(2*R)^1.011;
 
     % RANGE
-    alpha = (H_c+H_r)*R/(H_c+H_ro) %クレータ内縁
-    beta = R+(1-(H_c+H_r)/(H_c+H_ro))*W_r %クレータ外縁
+    alpha = (H_c+H_r)*R/(H_c+H_ro); %クレータ内縁
+    beta = R+(1-(H_c+H_r)/(H_c+H_ro))*W_r ;%クレータ外縁
     dist_to_zero =200; %標高ゼロまでの距離
-    A = -3*R^3 + 2*R^2*beta + 2*R*beta^2 + 2*beta^3
+    A = -3*R^3 + 2*R^2*beta + 2*R*beta^2 + 2*beta^3;
 
-    x_center = size_factor/2+100*randn(1)
-    y_center = size_factor/2+100*randn(1)
+    x_center = size_factor/2+100*randn(1);
+    y_center = size_factor/2+100*randn(1);
 
     %% 生成したクレータが近接した場合に削除する処理
     while 1
-        x_center_2 = 500*rand(1)
-        y_center_2 = 500*rand(1)
+        x_center_2 = 500*rand(1);
+        y_center_2 = 500*rand(1);
         if sqrt(abs(x_center-x_center_2)^2) + sqrt(abs(y_center-y_center_2)^2) > R
             break
         end
@@ -87,9 +87,10 @@ function f = put_crater(k,mode)
     model;
    %% ラベルデータとして保存
     label_data = zeros(size_factor,size_factor);
-    filenum = string(k)
-    filename = "two_craters_label/data_"+filenum
-    save(filename,'model')
+    label_data(model<-3)=1;
+    filenum = string(k);
+    filename = "two_craters_label/data_"+filenum;
+    save(filename,'label_data');
     %% 動画 v open
     if mode == 1
         v = VideoWriter('two_craters_image.avi')
@@ -97,18 +98,18 @@ function f = put_crater(k,mode)
     end
     %% mode2:三次元プロット
     if mode == 2
-    figure(1)
-    s = surface(model);
-    s.EdgeColor = 'none';
-    zlim([-100 100])
-    colorbar
-    view(3)
+        figure(1)
+        s = surface(model);
+        s.EdgeColor = 'none';
+        zlim([-100 100])
+        colorbar
+        view(3)
     end
     %%
-    time = 0
+    time = 0;
     lidar_data = zeros(size_factor,size_factor);
     time_data = zeros(size_factor,size_factor);
-    for i = 10:-1:-10
+    for i = 3:-1:-23
         time = time+1;
         lidar_data(model==i)=1;
         time_data(:,:,time) = lidar_data;
@@ -126,6 +127,6 @@ function f = put_crater(k,mode)
         close(v);
     end
     filenum = string(k)
-    filename = "two_craters_image/data_"+filenum
-    save(filename,'time_data')
+    filename = "two_craters_image/data_"+filenum;
+    save(filename,'time_data');
 end
