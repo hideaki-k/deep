4239afe2bfb824eaf5421d12f69852a2f9d52298
hideaki-k <h.kinoshita@kimura-lab.net>
2021-05-27 18:29:32 +0900

    Create unet_model.py

---
+++ pytorch_test/Surrogate_BP/model/unet_model.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes, bilinear=True, num_time=20):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+        self.num_time = num_time
+
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64,128)
+        self.down2 = Down(128,256)
+        self.down3 = Down(256,512)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024//factor)
+        self.up1 = Up(1024, 512//factor,bilinear)
+        self.up2 = Up(512, 256//factor, bilinear)
+        self.up3 = Up(256, 128//factor,bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, n_classes)
+
+    def forward(self, x):
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        out = torch.zeros((64,1,512,512), device=device, dtype=torch.float)
+        out_rec = [out]
+
+        self._reset_state()
+
+        for t in range(self.num_time):
+            x_t = x[:,:,t]
+            x_t = x_t.reshape((len(x_t),1,512,512))
+
+            x1 = self.inc(x)
+            x2 = self.down1(x1)
+            x3 = self.down2(x2)
+            x4 = self.down3(x3)
+            x5 = self.down4(x4)
+            x = self.up1(x5, x4)
+            x = self.up2(x, x3)
+            x = self.up3(x, x2)
+            x = self.up4(x, x1)
+            out = self.outc(x)
+            out_rec.append(out)
+
+        out_rec = torch.stack(out_rec,dim=1)
+        m = torch.sum(out_rec,1)
+
+        return m
+
+
+
+
