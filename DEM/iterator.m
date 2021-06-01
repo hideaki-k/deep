027769061e1a05acfa085faeb79ec7b5176e63d3
hideaki-k <h.kinoshita@kimura-lab.net>
2021-06-01 18:39:41 +0900

    Update iterator.m

--- DEM/iterator.m
+++ DEM/iterator.m
@@ -1,8 +1,8 @@
-for i=1:1:12800
-    f = put_crater(i,0);
+for i=1:1:1
+    f = put_crater(i,2);
    %f =  make_crater(i,2)
    %f =  make_crater(i,2)
-   %モード0:なし、モード1は動画生成、モード2：3次元プロット
+   %モード0:LOGデータ保存、モード1は動画生成、モード2：3次元プロット
 end
 
 function f = count(i)
