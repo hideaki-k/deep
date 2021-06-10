c737251772259d998919cca52e1641a1370bc13a
hideaki-k <h.kinoshita@kimura-lab.net>
2021-06-10 10:18:41 +0900

    Update unet_model.py

--- pytorch_test/Surrogate_BP/model/unet_model.py
+++ pytorch_test/Surrogate_BP/model/unet_model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from .unet_parts import *
+import matplotlib.pyplot as plt
 class UNet(nn.Module):
     def __init__(self, n_channels, n_classes, bilinear=True, num_time=10, gpu=True):
         super(UNet, self).__init__()
@@ -10,7 +11,7 @@ class UNet(nn.Module):
         self.bilinear = bilinear
         self.num_time = num_time
 
-        self.inc = DoubleConv(n_channels, 64)
+        self.inc = DoubleConv_1(n_channels, 64)
         self.down1 = Down(64,128)
         self.down2 = Down(128,256)
         self.down3 = Down(256,512)
@@ -36,7 +37,7 @@ class UNet(nn.Module):
 
     def forward(self, x_timeline):
         device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-        out = torch.zeros((x_timeline.shape[0],1,128,128), device=device, dtype=torch.float)
+        out = torch.zeros((x_timeline.shape[0],1,64,64), device=device, dtype=torch.float)
         out_rec = [out]
 
         self._reset_state()
@@ -45,10 +46,15 @@ class UNet(nn.Module):
 
             #print('x shape:',x_timeline.shape) #([2, 512, 512, 21])
             x_t = x_timeline[:,:,:,t]
+            x_t_=x_t.to('cpu').detach().numpy().copy()
+            print("x_t_ shape",x_t_.shape)
+            plt.imshow(x_t_[0,:,:])
+            plt.show()
             #print("x_t",x_t.shape)
-            x_t = x_t.reshape((len(x_t),1,128,128)) 
+            x_t = x_t.reshape((len(x_t),1,64,64)) 
             #print('x_t shape',x_t.shape)
-            x1 = self.inc(x_t)
+            x1 = self.inc(x_t) #([2, 64, 128, 128])
+
             x2 = self.down1(x1)
             x3 = self.down2(x2)
             x4 = self.down3(x3)
