c1c26b062388f9b547120b7592e472dafae8adb1
hideaki-k <h.kinoshita@kimura-lab.net>
2021-07-04 16:29:47 +0900

    rec接続の追加

--- pytorch_test/snu/model/snu_layer.py
+++ pytorch_test/snu/model/snu_layer.py
@@ -18,7 +18,8 @@ from . import step_func
 class SNU(nn.Module):
     def __init__(self, in_channels, out_channels, l_tau=0.8, soft=False, rec=False, nobias=False, initial_bias=-0.5, gpu=True):
         super(SNU,self).__init__()
-        
+
+        self.in_channels = in_channels
         self.out_channels= out_channels
         self.l_tau = l_tau
         self.rec = rec
@@ -40,7 +41,8 @@ class SNU(nn.Module):
         #torch.nn.init.normal_(self.w1, mean=0.0)
         
         #self.Wx = torch.einsum("abc,cd->abd", (x_data, w1))
-        self.Wx = nn.Linear(4374, out_channels, bias=False).to(device)
+        #self.Wx = nn.Linear(4374, out_channels, bias=False).to(device)
+        self.Wx = nn.Linear(726, out_channels, bias=False).to(device)
         #nn.init.uniform_(self.Wx.weight, -0.1, 0.1) #3.0
         torch.nn.init.xavier_uniform_(self.Wx.weight)
 
@@ -97,7 +99,6 @@ class SNU(nn.Module):
 
         else:
             axis = 0
-
             #print("s.shape:", s.shape)
             #print("self.b.shape:", self.b.shape)
             #print("self.initial_bias.shape:",self.initial_bias.shape)
@@ -106,9 +107,7 @@ class SNU(nn.Module):
             #print("bias:",bias)
             #print("s in snu:",s)
             bias = s + self.b
-            #bias = s + self.initial_bias
-            
-            #print("bias in snu:",bias)
+
             y = step_func.spike_fn(bias)
         
         self.s = s
@@ -140,20 +139,19 @@ class Conv_SNU(nn.Module):
         else:
             dtype = torch.float
             device=torch.device("cpu")
-        
 
         self.Wx = nn.Conv2d(self.in_channels, self.out_channels, kernel_size=self.kernel_size, stride=self.stride, padding=self.padding, bias=False).to(device) #入力チャネル数, 出力チャネル数, フィルタサイズ
         torch.nn.init.xavier_uniform_(self.Wx.weight)
 
+        if self.rec:
+            self.Wy = nn.Conv2d(self.out_channels, self.out_channels, kernel_size=self.kernel_size, stride=self.stride, padding=self.padding, bias=False).to(device) #入力チャネル数, 出力チャネル数, フィルタサイズ
+            torch.nn.init.xavier_uniform_(self.Wy.weight)
+
         if nobias:
             self.b = None
         else:
-
-            #print("initial_bias",initial_bias)
             device = torch.device(device)
-            
             self.b = nn.Parameter(torch.Tensor([initial_bias]).to(device))
-            #print("self.b",self.b)
 
     def reset_state(self, s=None, y=None):
         self.s = s
@@ -161,7 +159,6 @@ class Conv_SNU(nn.Module):
 
     def initialize_state(self, shape): #shape (バッチ,tチャネル,oh,ow)
         if self.gpu:
-            #xp = cuda.cupy
             dtype = torch.float
             device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         else:
@@ -175,28 +172,23 @@ class Conv_SNU(nn.Module):
         ############dem_autoencoder_segmentation.py
         #self.s = torch.zeros((shape[0], self.out_channels, shape[2], shape[3]),device=device,dtype=dtype)
         #self.y = torch.zeros((shape[0], self.out_channels, shape[2], shape[3]),device=device,dtype=dtype)
-        
+        #self.Wrs = nn.Parameter(torch.empty((shape[0], self.out_channels, self.oh, self.ow),device=device,dtype=dtype))
+        #self.br = nn.Parameter(torch.empty((shape[0], self.out_channels, self.oh, self.ow),device=device,dtype=dtype))
     
     def forward(self,x):
         if self.s is None:
-            #print("self.s is none")
             self.initialize_state(x.shape)
-
 
         if type(self.s) == numpy.ndarray:
             self.s = torch.from_numpy(self.s.astype(np.float32)).clone()
-    
-        #print("x in snu:",x) 
-        #print("x in snu.shape",x.shape) #([256, 4096])
-        #print("self.Wx(x).shape",self.Wx(x).shape)
-        
-        #print("self.Wx(x).shape",self.Wx(x).shape)
-        #print("self.s.shape : ",self.s.shape)
-        #print( " self.l_tau * self.s * (1-self.y)):",self.l_tau * self.s * (1-self.y))
-        #print("self.Wx(x):",self.Wx(x))
-        
-        s = F.elu(abs(self.Wx(x)) + self.l_tau * self.s * (1-self.y))
-        
+       # print("=rec:",self.rec)
+        #print('=self.Wy(self.y)',self.Wy(self.y).shape)
+        #print('=self.Wx(x)',self.Wx(x).shape)
+        if self.rec:
+            s = F.elu(abs(self.Wx(x)) + self.Wy(self.y) + self.l_tau * self.s * (1-self.y))
+        else:
+            s = F.elu(abs(self.Wx(x)) + self.l_tau * self.s * (1-self.y))
+        #s = F.elu(abs(self.Wx(x)) + r * self.s * (1-self.y))
 
         if self.soft:
 
@@ -207,18 +199,8 @@ class Conv_SNU(nn.Module):
 
         else:
             axis = 0
-
-            #print("s.shape:", s.shape)
-            #print("self.b.shape:", self.b.shape)
-            #print("self.initial_bias.shape:",self.initial_bias.shape)
-            #print("self.b.shape !!!!!!!!!!!!!!!! ", self.b[(...,) + (None,) * (s.ndim - self.b.ndim - axis)].shape)
             bias = s + self.b[(...,) + (None,) * (s.ndim - self.b.ndim - axis)] #error!! two types
-            #print("bias:",bias)
-            #print("s in snu:",s)
             bias = s + self.b
-            #bias = s + self.initial_bias
-            
-            #print("bias in snu:",bias)
             y = step_func.spike_fn(bias)
         
         self.s = s
@@ -254,6 +236,11 @@ class tConv_SNU(nn.Module):
 
         self.Wx = nn.ConvTranspose2d(self.in_channels, self.out_channels, self.kernel_size, self.stride, bias=False).to(device) #入力チャネル数, 出力チャネル数, フィルタサイズ
         torch.nn.init.xavier_uniform_(self.Wx.weight)
+        if self.rec:
+
+            self.Wy = nn.Conv2d(self.out_channels, self.out_channels, kernel_size=3, padding=1, stride=1, bias=False).to(device) #入力チャネル数, 出力チャネル数, フィルタサイズ
+            #self.Wy = nn.Linear(self.out_channels, self.out_channels)
+            torch.nn.init.xavier_uniform_(self.Wy.weight)
 
         if nobias:
             self.b = None
@@ -261,7 +248,6 @@ class tConv_SNU(nn.Module):
 
             #print("initial_bias",initial_bias)
             device = torch.device(device)
-            
             self.b = nn.Parameter(torch.Tensor([initial_bias]).to(device))
             #print("self.b",self.b)
 
@@ -280,27 +266,25 @@ class tConv_SNU(nn.Module):
             
         self.s = torch.zeros((shape[0], self.out_channels, 2*shape[2], 2*shape[3]),device=device,dtype=dtype)
         self.y = torch.zeros((shape[0], self.out_channels, 2*shape[2], 2*shape[3]),device=device,dtype=dtype)
-        
+        #self.Wrs = nn.Parameter(torch.empty((shape[0], self.out_channels, 2*shape[2], 2*shape[3]),device=device,dtype=dtype))
+        #self.br = nn.Parameter(torch.empty((shape[0], self.out_channels, 2*shape[2], 2*shape[3]),device=device,dtype=dtype))
     
     def forward(self,x):
         if self.s is None:
             #print("self.s is none")
             self.initialize_state(x.shape)
-
-
+        #print(self.l_tau)
+        #print("rec:",self.rec)
+        #print('self.Wy(self.y)',self.Wy(self.y).shape)
+        #print('self.Wx(x)',self.Wx(x).shape)
         if type(self.s) == numpy.ndarray:
             self.s = torch.from_numpy(self.s.astype(np.float32)).clone()
     
-        #print("x in snu:",x) 
-        #print("x in snu.shape",x.shape) #x in snu.shape torch.Size([256, 784])
-        #print("self.Wx(x).shape",self.Wx(x).shape)
-        
-        #print("self.Wx(x).shape",self.Wx(x).shape)
-        #print("self.s.shape : ",self.s.shape)
-        #print( " self.l_tau * self.s * (1-self.y)):",self.l_tau * self.s * (1-self.y))
-        #print("self.Wx(x):",self.Wx(x))
-        
-        s = F.elu(abs(self.Wx(x)) + self.l_tau * self.s * (1-self.y))
+        if self.rec:
+            s = F.elu(abs(self.Wx(x)) + self.Wy(self.y) + self.l_tau * self.s * (1-self.y))
+        else:
+            s = F.elu(abs(self.Wx(x)) + self.l_tau * self.s * (1-self.y))
+        #s = F.elu(abs(self.Wx(x)) + r * self.s * (1-self.y))
         
 
         if self.soft:
