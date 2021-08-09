6084fa4df7e4f456134395b6353c385d8e682c5c
hideaki-k <h.kinoshita@kimura-lab.net>
2021-08-09 17:58:53 +0900

    Update hazard_detect.py

--- DEM/hazard_detect.py
+++ DEM/hazard_detect.py
@@ -3,55 +3,93 @@ import scipy.io
 import numpy as  np
 import matplotlib.pyplot as plt
 import cv2
+import os
 
-files = glob.glob("128pix_(0deg)_dem/model/*.mat")
-height = scipy.io.loadmat(files[0])['model'].shape[0]
-width = scipy.io.loadmat(files[0])['model'].shape[1]
+new_dir_path = r'C:/Users/aki/Documents/GitHub/deep/DEM/64pix_(0deg)_dem(noisy)/hazard_label'
+os.makedirs(new_dir_path, exist_ok=True)
+files = glob.glob(r'C:/Users/aki/Documents/GitHub/deep/DEM/64pix_(0deg)_dem(noisy)/model/*.mat')
+
+rei = scipy.io.loadmat(files[1])['DEM']
+print(rei)
+height = rei.shape[0]
+width = rei.shape[1]
+# ウィンドウ大きさ
 F = 8
 
 
-def Get_Slope(h, w, D):
-    roi = D[(h-F//2):(h+F//2),(w-F//2):(w+F//2)]
-    #  laplacian =  cv2.Laplacian(D,cv2.CV_64F)
-    slope = np.amax(roi) - np.amin(roi)
+def Get_Slope(h, w, D, mu, sigma):
 
-    return slope
-
-def Get_Roughness(h, w, D):
     roi = D[(h-F//2):(h+F//2),(w-F//2):(w+F//2)]
-    return np.mean(roi)
+    mu_roi = np.mean(roi)
+    suiheido = (mu_roi - mu)/sigma
+    return abs(suiheido)
+
+def Get_Roughness(h, w, D, mu, sigma):
+
+    roi = D[(h-F//2):(h+F//2),(w-F//2):(w+F//2)]
+    s_roi = np.std(roi)
+    heitando = s_roi/sigma
+    return heitando
 
 print(height,width)
-V = np.zeros((height,width)) # safety for each pixel
-S = np.zeros((height,width)) # slope for each pixel
-R = np.zeros((height,width)) # roughness for each pixel
+
 
 for file in files:
-    DEM = scipy.io.loadmat(file)['model']
+    print('input : ',file)
+    target = '\model_'
+    idx = file.find(target)
+    cnt = file[idx+7:]
+    idx = cnt.find('.mat')
+    cnt = cnt[:idx]
+    DEM = scipy.io.loadmat(file)['DEM'] 
+
+    mu = np.mean(DEM)
+    sigma = np.std(DEM)
+    print("heikin ",mu)
+
+ 
     laplacian =  cv2.Laplacian(DEM,cv2.CV_64F)
-    #lt.imshow(laplacian)
-    #plt.show()
+    V = np.zeros((height,width)) # safety for each pixel
+    S = np.zeros((height,width)) # slope for each pixel
+    R = np.zeros((height,width)) # roughness for each pixel
     
-
-
     for row in range(F//2, height-(F//2), 1):
         for col in range(F//2, width-(F//2), 1):
-            s = Get_Slope(row, col, DEM)
-            r = Get_Roughness(row, col, DEM)
-            if s > S[row][col]:
-                S[row][col] = s
-            if r > R[row][col]:
-                R[row][col] = r
-    #print(S)
+            suiheido = Get_Slope(row, col, DEM, mu, sigma)
+            heitando = Get_Roughness(row, col, DEM, mu, sigma)
+            if suiheido > S[row][col]:
+                S[row][col] = suiheido
+            if heitando > R[row][col]:
+                R[row][col] = heitando
+    
+    Vthm = np.mean(S)
+    Vths = np.mean(R)
+    S = S>Vthm
+    R = R>1.5*Vths
+    hazard = (S|R)
+
+    save_path = new_dir_path + '/label_'+ cnt + '.mat'
+    print('output : ',save_path)
+    scipy.io.savemat(save_path, {'label_data':hazard})
+
+    #np.set_printoptions(threshold=np.inf)
+    #print(S+R)
+ 
+    
     fig = plt.figure()
-    ax1 = fig.add_subplot(1,3,1)
-    ax2 = fig.add_subplot(1,3,2)
-    ax3 = fig.add_subplot(1,3,3)
+    ax1 = fig.add_subplot(1,4,1)
+    ax2 = fig.add_subplot(1,4,2)
+    ax3 = fig.add_subplot(1,4,3)
+    ax4 = fig.add_subplot(1,4,4)
     ax1.imshow(DEM)
+    ax2.set_title('suiheido')
     ax2.imshow(S) 
-    ax3.imshow(S>3)
+    ax3.imshow(hazard)
+    ax4.set_title('heitando')
+    ax4.imshow(R)
     fig.savefig('figure01.jpg')
-    #plt.show()
-    break
+    plt.show()
+  
+    
 
     
