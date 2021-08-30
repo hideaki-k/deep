e48494af650bd9a68059411b5f35055649d6c16e
hideaki-k <h.kinoshita@kimura-lab.net>
2021-08-30 18:01:46 +0900

    Update fractal_terrain_generation.m

--- DEM/fractal_terrain_generation.m
+++ DEM/fractal_terrain_generation.m
@@ -1,16 +1,6 @@
-function f = fractal_terrain_generation(k,mode,pix,angle,folder_name,is_noise)
+function f = fractal_terrain_generation(k,mode,pix,angle,folder_name,is_noise,is_boulder)
     f = mode;
-%% generate background fractal terrain
-    x = [0:0.1:64];
-    y = [0:0.1:64];
-%% Generate the random terrain with Perlin noise
-    if is_noise == true
-        % Initial calculation
-        [X, Y] = meshgrid(x,y);
-        f = @(t) myinterpolation(t);
-        H = perlin_2d(f, 1, X, Y);
-        size(H);
-    end
+
 %% 斜面 & フラクタルの付与
     size_factor = pix;
     time_scale = 20;
@@ -36,39 +26,63 @@ function f = fractal_terrain_generation(k,mode,pix,angle,folder_name,is_noise)
             end
         end
     end
-    % フラクタル化
-     noise_min = 0;
-     noise_max = 0.8;
-     noise_val = (noise_max-noise_min).*rand(1)+noise_min;
-    for i=1:1:size_factor
-        for j=1:1:size_factor
-            base(i,j) = base(i,j) + noise_val*H(i,j);
-        end
-    end
+
    %% クレータの個数 & 座標を決定
-   center_x_list = []; %　クレータ中心座標_x
-   center_y_list = []; % クレータ中心座標_y
-   crater_num = 1 + (1 + 10)*rand(1) %クレータ個数(1~10)
+   crater_num = round(1 + (1 + 6)*rand(1)); % クレータ個数(1~6)
+   center_x_list = zeros(crater_num,1); %　クレータ中心座標_x
+   center_y_list = zeros(crater_num,1); % クレータ中心座標_y
+   
    alpha = [];
-   R = [];
-   for crater = 1:1:crater_num
-        %% set parameter
-        R(end+1) = 3 + (-3 + 15)*rand(1) %クレータ半径(3~10)
-
-
-   end
+   R = zeros(crater_num,1);
 
    for crater = 1:1:crater_num
+       R(crater) = 3 + (-3 + 15)*rand(1); %クレータ半径(3~10)
        x_cord = 3 + (3 + 64)*rand(1);
        y_cord = 3 + (3 + 64)*rand(1);
-       center_x_list(end+1) = round(x_cord);
-       center_y_list(end+1) = round(y_cord);
+       center_x_list(crater) = round(x_cord);
+       center_y_list(crater) = round(y_cord);
    end
+   %% ボルダーの個数 & 座標を決定
+
+    boulder_num = round(5*rand(1)); % ボルダー個数(0~5)
+    boulder_center_x_list =  zeros(boulder_num,1); % ボルダー中心座標x
+    boulder_center_y_list = zeros(boulder_num,1); % ボルダー中心座標y
+    boulder_xziku_list = zeros(boulder_num,1); % ボルダーx軸長さ
+    boulder_yziku_list = zeros(boulder_num,1); % ボルダーy軸長さ
+    boulder_zziku_list = zeros(boulder_num,1); % ボルダーz軸長さ
    
-    [DEM,label_data] = put_crater(base, center_x_list, center_y_list, R);
-    
-    if mode ~= 2     
+   if is_boulder
+   for boulder = 1:1:boulder_num
+       % 座標
+       x_cord = 3 + (3 + 64)*rand(1);
+       y_cord = 3 + (3 + 64)*rand(1);
+       boulder_center_x_list(boulder) = round(x_cord);
+       boulder_center_y_list(boulder) = round(y_cord);
+       % 大きさ
+       xr = abs(5*rand(1));
+       yr = abs(5*rand(1));
+       zr = 3+abs(1*rand(1));
+       boulder_xziku_list(boulder) = round(xr);
+       boulder_yziku_list(boulder) = round(yr);
+       boulder_zziku_list(boulder) = round(zr);
+   end
+
+   end
+
+    DEM = put_hazard(base,is_noise, center_x_list, center_y_list, R, is_boulder, boulder_center_x_list, boulder_center_y_list, boulder_xziku_list, boulder_yziku_list, boulder_zziku_list);
+   
+   
+   
+    %% 描画モード（丸め）
+    if mode ~= 2  
+    true_DEM = DEM;    
     DEM = round(DEM,0);
+    end
+    %% 動画 v open
+
+    if mode == 1
+        v = VideoWriter('SLOPE_two_craters_image.avi')
+        open(v)
     end
    %% 三次元プロット
     if mode==2
@@ -83,16 +97,16 @@ function f = fractal_terrain_generation(k,mode,pix,angle,folder_name,is_noise)
         view(3)
         savefig('model.fig')
        
-        figure(2)
-        s = surf(label_data);
-        s.EdgeColor = 'none';
-        xlabel('X');
-        ylabel('Y');
-        zlim([-10 24])
-        colormap turbo
-        colorbar
-        view(3)
-        savefig('label.fig')
+%         figure(2)
+%         s = surf(label_data);
+%         s.EdgeColor = 'none';
+%         xlabel('X');
+%         ylabel('Y');
+%         zlim([-10 24])
+%         colormap turbo
+%         colorbar
+%         view(3)
+%         savefig('label.fig')
         
     end
     time = 0;
@@ -101,7 +115,7 @@ function f = fractal_terrain_generation(k,mode,pix,angle,folder_name,is_noise)
     max_elevation = max(DEM(:));
     min_elevation = max_elevation-10;
     for i = max_elevation:-1:min_elevation
-        i;
+
         time = time+1;
         lidar_data(DEM==i)=1;
         time_data(:,:,time) = lidar_data;
@@ -121,15 +135,29 @@ function f = fractal_terrain_generation(k,mode,pix,angle,folder_name,is_noise)
 
     %% 教師データとして保存
     if mode == 0
-
+            
         filenum = string(k);
+        % true_DEM.mat hazard_label評価用
+        filename = folder_name+"/model/real_model_"+filenum;
+        save(filename,'true_DEM');
+        
+        % observed_DEM.mat hazard_label評価用 8/29追加
+        filename = folder_name+"/model/observed_model_"+filenum;
+        save(filename,'DEM');
+        
+        % model_png 見た目で評価（丸め、なしとあり）
+        kyorigazou = mat2gray(true_DEM);
+        filename = folder_name+"/model/model_true"+filenum+'.png';
+        imwrite(kyorigazou,filename);
         kyorigazou = mat2gray(DEM);
         filename = folder_name+"/model/model_"+filenum+'.png';
         imwrite(kyorigazou,filename);
         
+        % label_data.mat crater中心を塗りつぶした
         filename = folder_name+"/label/label_"+filenum;
         save(filename,'label_data');
      
+        % time_data lidar再現データ
         filename = folder_name+"/image/image_"+filenum
         save(filename,'time_data');
     end
