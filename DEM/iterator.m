cdde3fd175b144efb02023b717b6feb251623e60
hideaki-k <h.kinoshita@kimura-lab.net>
2021-09-13 13:00:25 +0900

    mix_angle

--- DEM/iterator.m
+++ DEM/iterator.m
@@ -3,20 +3,28 @@ clear all
 close all
 clc
 pix = 64;
-angle = 0;
+max_angle = 5;
+is_mixangle = true;
 is_noise = true;
 is_boulder = false;
 if is_noise
-    folder_name = string(pix)+"pix_("+string(angle)+"deg)_dem(noisy)_ver2";
+    %folder_name = string(pix)+"pix_("+string(angle)+"deg)_dem(noisy)_ver2";
+    %folder_name = string(pix)+"pix_("+string(angle)+"deg)_dem(noisy)"; % 9/5
+    if is_mixangle;
+        folder_name = string(pix)+"pix_(0-"+string(max_angle)+"deg)_dem(noisy)"; % 9/13
+    end
 else
-    folder_name = string(pix)+"pix_("+string(angle)+"deg)_dem_ver2";
+    %folder_name = string(pix)+"pix_("+string(angle)+"deg)_dem_ver2";
 end
 mkdir(folder_name);
 mkdir(folder_name,'image');
 mkdir(folder_name,'label');
 mkdir(folder_name,'model');
-% 100:1:7680
-for i=7680:1:16640
+% 7680:1:16640
+for i=0:1:7680
+   if is_mixangle;
+       angle = round((max_angle)*rand(1)) % スロープ角(1~5)
+   end
    f = fractal_terrain_generation(i,0,pix,angle,folder_name,is_noise,is_boulder);
 %   f = base_DEM(i,2,pix,angle,folder_name,is_noise);
 
