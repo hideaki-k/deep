7bff7853453e8464afe270d74d310276f737a9d2
hideaki-k <h.kinoshita@kimura-lab.net>
2022-01-27 11:14:37 +0900

    Update dem_3d_plot.m

--- DEM/dem_3d_plot.m
+++ DEM/dem_3d_plot.m
@@ -1,17 +1,45 @@
 clear
 close all
-filename = 'C:\Users\aki\OneDrive - 東京理科大学\研究\64pix_(0-5deg)_dem(noisy)\model\real_model_396.mat'
+filename = 'C:\Users\aki\Documents\GitHub\deep\DEM\64pix_(0-3deg)_dem(lidar_noisy)_boulder\model\real_model_5413.mat'
+%filename = 'C:\Users\aki\OneDrive - 東京理科大学\研究\64pix_(0deg)_dem(noisy)_evaluate\model\real_model_549.mat'
+%filename ='C:\Users\aki\OneDrive - 東京理科大学\研究\real_model_36.mat'
+
 DEM = load(filename,'true_DEM');
 DEM = DEM.true_DEM;
+% DEM = load(filename,'Lidar_noised_DEM');
+% DEM = DEM.Lidar_noised_DEM;
 
-figure(1)
+%%Interpolant
+sz = size(DEM)
+xg = 1:sz(1);
+yg = 1:sz(2);
+F = griddedInterpolant({xg,yg},double(DEM));
+
+xq = (0:5/15:sz(1))';
+yq = (0:5/15:sz(2))';
+vq = (F({xq,yq}));
+figure(1);
+s = surf(vq);
+
+s.EdgeColor = 'none';
+xlim([0 200])
+ylim([0 200])
+zlim([-32 32])
+colormap bone
+c = colorbar;
+view(3)
+
+figure(2)
 s = surf(DEM);
 s.EdgeColor = 'none';
 xlabel('X[m]');
 ylabel('Y[m]');
 zlabel('Altitude[m]');
-zlim([-20 20])
-colormap turbo
+xlim([0 64])
+ylim([0 64])
+zlim([-32 32])
+colormap copper
 c = colorbar;
-c.Label.String = 'm';
+c.Label.String = 'Altitude[m]';
 view(3)
+
